53a97bbb84c876ef57ec58bc3b65ea3236b01484
Nitish Kumar <nitishonspeed@gmai.com>
2017-12-02 12:49:01 +0530

    pascal patterns

--- patterns/pascalTriangle.c
+++ patterns/pascalTriangle.c
@@ -5,44 +5,41 @@ int combi(int a,int b);
 void pascale(int a);
 int main (){
    pascale(7);
-      
-               
-	              return 0;
-		      }
+   
+         
+       return 0;
+}
 
-		      void pascale(int line){
-		                  int i,j,k,r;
-				            for(i=1;i<=line;i++){ 
-					                      k=1;
-							                        r=0;
-										               for(j=1;j<=line*2-1;j++){
-											                          
-														                         if(j>=line+1-i&&j<=line-1+i&&k==1){
-																	                                 cout<<combi(i-1,r);
-																					                                         k=0;
-																										                                        r++;
-																															                               }
-																																		                                       else{
-																																						                                        cout<<" ";
-																																											                                k=1;
-																																															                                }
-																																																			                       }
-																																																					                               cout<<"\n";
-																																																								                       
-																																																										                 }
-																																																												 }
-																																																												 int combi(int n, int r){
-																																																												         
-																																																													         return (fact(n)/fact(r)/fact(n-r));        
-																																																														 }
+void pascale(int line){
+            int i,j,k,r;
+          for(i=1;i<=line;i++){ 
+                  k=1;
+                  r=0;
+               for(j=1;j<=line*2-1;j++){
+                   
+                       if(j>=line+1-i&&j<=line-1+i&&k==1){
+                                cout<<combi(i-1,r);
+                                        k=0;
+                                       r++;
+                               }
+                                else{
+                                 cout<<" ";
+                                k=1;
+                                }
+                       }
+                        cout<<"\n";
+                
+          }
+}
+int combi(int n, int r){
+        
+        return (fact(n)/fact(r)/fact(n-r));        
+}
 
-																																																														 int fact(int a){
-																																																														         
-																																																															         if(a==1||a==0)
-																																																																         return 1;
-																																																																	         else
-																																																																		         return a*fact(a-1);
-																																																																			 }
-
-
-
+int fact(int a){
+        
+        if(a==1||a==0)
+        return 1;
+        else
+        return a*fact(a-1);
+}
